ebc02ea7a83ea9ff0def424fc6e15b4e54edd6b7
Guilherme Santana <guiguilsantana@gmail.com>
2024-03-16 23:32:59 -0300

    feat: Modifying EmailConsumer

--- src/main/java/com/ms/email/consumers/EmailConsumer.java
+++ src/main/java/com/ms/email/consumers/EmailConsumer.java
@@ -1,15 +1,24 @@
 package com.ms.email.consumers;
 
 import com.ms.email.dto.EmailRecordDto;
+import com.ms.email.models.EmailModel;
+import com.ms.email.services.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailConsumer {
+    final EmailService emailService;
 
+    public EmailConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void ListenEmailQueue(@Payload EmailRecordDto emailRecordDto){
-        System.out.println(emailRecordDto.email());
+        var emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailRecordDto, emailModel);
+        emailService.sendEmail(emailModel);
     }
 }
